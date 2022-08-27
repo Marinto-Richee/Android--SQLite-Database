f41c5368a662923d245f3d3a27d00fac40a20317
Marinto Richee <65499285+Marinto-Richee@users.noreply.github.com>
2022-08-27 13:24:02 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/sqlliteapp/MainActivity.java
+++ app/src/main/java/com/example/sqlliteapp/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.sqlliteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editUserID;
+    EditText editUserName;
+    EditText editUserPassword;
 
+    DatabaseManager dbManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editUserID= findViewById(R.id.editTextID);
+        editUserName=findViewById(R.id.editTextUserName);
+        editUserPassword=findViewById(R.id.editTextPassword);
+
+        dbManager=new DatabaseManager(this);
+        try{
+            dbManager.open();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void btnInsertPressed(View v){
+        dbManager.insert(editUserName.getText().toString(),editUserPassword.getText().toString());
+    }
+
+    public void btnFetchPressed(View v){
+        Cursor cursor=dbManager.fetch();
+
+    }
+
+    public void btnUpdatePressed(View v){
+
+    }
+
+    public void btnDeletePressed(View v){
+
     }
 }
